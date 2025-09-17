1a6646e78b6a80f2ddf8329826cd0622bd1fcf20
Coby Jennings <cobyj.jennings@gmail.com>
2025-09-17 21:31:51 +1000

    Rapidjson

---
+++ Source/Core/LevelLoader.cpp
@@ -0,0 +1,5 @@
+﻿//
+// Created by addic on 17/09/2025.
+//
+
+#include "LevelLoader.h"
---
+++ Source/Core/LevelLoader.h
@@ -0,0 +1,18 @@
+﻿//
+// Created by addic on 17/09/2025.
+//
+
+#ifndef RAYLIBFPS_LEVELLOADER_H
+#define RAYLIBFPS_LEVELLOADER_H
+#include "World.h"
+#include <rapidjson/document.h>
+
+class LevelLoader
+{
+public:
+	std::unique_ptr<World> Load(const char* Path);
+	void ParseNode(World* _World, const rapidjson::Document Doc);
+};
+
+
+#endif //RAYLIBFPS_LEVELLOADER_H
